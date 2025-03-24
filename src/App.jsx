4e6316e8ff7180349035236903a93a6f2c5624ae
orionchik182 <banjik13@gmail.com>
2025-03-25 00:30:13 +0400

    feat: add basename in browserRouter-3

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
     <div>
       <Header />
       <Routes>
-        <Route index element={<ProductList />} />
-        <Route path={"form"} element={<Form />} />
+        <Route path="/" element={<ProductList />} />
+        <Route path="/form" element={<Form />} />
       </Routes>
     </div>
   );
