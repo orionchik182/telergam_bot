cbbc49749ede743ecda2d318dc34591499ec1cbf
orionchik182 <banjik13@gmail.com>
2025-03-25 13:43:25 +0400

    feat: add basename in browserRouter-17

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import "./stile.css";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter as Route, Routes } from "react-router-dom";
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
